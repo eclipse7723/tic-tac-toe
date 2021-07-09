8b2134315c9909386ceb7313c9d7f72dee5bf26a
eclipse7723 <eclipse7723@gmail.com>
2021-07-09 03:20:21 +0300

    ver. 2.0 # realised gameover # more comments

--- App.py
+++ App.py
@@ -1,8 +1,9 @@
 from random import sample
 
 
-EMPTY_CELL = '.'
-MARKERS = ['X', 'O']
+EMPTY_CELL = '.'        # Маркер пустой ячейки
+MARKERS = ['X', 'O']    # Маркеры игроков
+DESK_SIZE = 3           # Размер доски (поддерживается сейчас только 3)
 
 
 class Player:
@@ -13,19 +14,59 @@ class Player:
         self.name = name
 
 
-class Desk:     # Model
+class Desk:
     def __init__(self):
-        self.__desk = [[EMPTY_CELL for i in range(3)] for i in range(3)]
+        self.__desk = [[EMPTY_CELL for i in range(DESK_SIZE)] for i in range(DESK_SIZE)]
         self.__players = []
         self.__step = 0             # четные для первого игрока, нечетные для второго
+        self.__state = False, None  # Завершена ли игра, Победитель
 
     def get_desk(self):
+        """ Возвращает доску """
         return self.__desk
 
+    def update_desk(self, marker: str, position: tuple):
+        """ Устанавливает маркер в указаную позицию """
+        if self.__desk[position[0]][position[1]] == EMPTY_CELL:
+            self.__desk[position[0]][position[1]] = marker
+            return True
+        else:
+            return False
+
+    @property
+    def state(self):
+        return self.__state
+
+    def check_combinations(self, lst: list):
+        """ Проверка выигрышной комбинации """
+        for row in lst:
+            if row[0] == EMPTY_CELL:
+                continue
+            if len(set(row)) == 1:
+                self.__state = True, self.fetch_player_by_marker(row[0])
+
     def check_desk(self):
-        """ TODO: Проверяет доску на предмет чьей-то победы """
-        state = False, None     # Завершена ли игра, Победитель
-        return state
+        """ Проверяет доску на предмет чьей-то победы """
+        # 1) Есть ли свободные ячейки
+        isEmpty = False
+        for row in self.__desk:
+            if EMPTY_CELL in row:
+                isEmpty = True
+                break
+        if isEmpty is False:
+            # Игра завершена, но победителя нет
+            self.__state = True, None
+
+        # 2) Поиск выиграшных комбинаций
+        rows = [row for row in self.__desk]
+        columns = [[row[i] for row in self.__desk] for i in range(DESK_SIZE)]
+        diagonals = [[self.__desk[i][i] for i in range(DESK_SIZE)],                 # Главная диагональ
+                     [self.__desk[i][DESK_SIZE-i-1] for i in range(DESK_SIZE)]]     # Побочная диагональ
+        self.check_combinations(rows)
+        self.check_combinations(columns)
+        self.check_combinations(diagonals)
+
+        return self.__state
 
     @property
     def players(self):
@@ -37,17 +78,10 @@ class Desk:     # Model
         self.__players.append(player)
 
     def fetch_player_by_marker(self, marker):
+        """ Возвращает игрока по указанному маркеру """
         assert marker.upper() in MARKERS, f'Unknown marker {marker!r}, it should be {MARKERS[0]!r} or {MARKERS[1]!r}'
         players = {player.marker: player for player in self.__players}
         return players[marker.upper()]
-
-    def update_desk(self, marker: str, position: tuple):
-        """ Устанавливает маркер в указаную позицию """
-        if self.__desk[position[0]][position[1]] == EMPTY_CELL:
-            self.__desk[position[0]][position[1]] = marker
-            return True
-        else:
-            return False
 
     @property
     def step(self):
@@ -73,7 +107,7 @@ class Controller:
         self.view = view
 
     def init_players(self):
-        """ Создаёт игроков """
+        """ Создаёт игроков и показывает инструкцию """
         self.view.notify("Create your players")
         self.view.send("Enter 1st player's name")
         nameA = input("> ")
@@ -82,14 +116,16 @@ class Controller:
         markerA, markerB = sample(MARKERS, 2)
         playerA = Player(nameA, markerA)
         playerB = Player(nameB, markerB)
-        self.view.notify(f"1st player's name is {playerA.name} and his marker is {playerA.marker}")
-        self.view.notify(f"2nd player's name is {playerB.name} and his marker is {playerB.marker}")
+        self.view.notify(f"1st player's name is {playerA.name} and his marker is {playerA.marker!r}")
+        self.view.notify(f"2nd player's name is {playerB.name} and his marker is {playerB.marker!r}")
         self.show_instruction()
         self.desk.add_player(playerA)
         self.desk.add_player(playerB)
 
     def read(self):
         """ Читает команды от пользователя """
+        if self.desk.state[0]:
+            return False
         command = input("> ").replace(' ', '').lower()
         allowed_moves = self.desk.get_allowed_moves()
         if command in allowed_moves.keys():
@@ -104,6 +140,7 @@ class Controller:
             exit()
         else:
             self.view.notify("Unknown command, check 'help'")
+        return True
 
     def show_desk(self):
         """ Отобразить доску """
@@ -117,12 +154,11 @@ class Controller:
         self.desk.next_step()
         self.view.notify(f"{player.name} makes a move:")
         self.view.draw(self.desk.get_desk())
-        winner = self.desk.check_desk()
-        if winner[0]:
-            self.show_results(winner)
+        # Проверяем состояние игры (то есть доски)
+        self.check_game_results()
 
     def show_instruction(self):
-        """ Отображает инструкцию для команды 'help' """
+        """ Отображает инструкцию -> 'help' """
         self.view.instructions()
 
     def get_current_player(self):
@@ -130,13 +166,21 @@ class Controller:
         return self.desk.players[self.desk.step % 2]
 
     def show_current_player(self):
-        """ Напоминает, кто сейчас ходит | для команды 'who' """
+        """ Напоминает, кто сейчас ходит -> 'who' """
         current_player = self.get_current_player()
-        self.view.notify(f"Current player is {current_player.name}")
+        self.view.notify(f"Current player is {current_player.name} (marker: {current_player.marker!r})")
 
-    def show_results(self, winner):
+    def check_game_results(self):
+        """ Возвращает состояние доски """
+        # Проверяем доску только после пятого хода, поскольку до этого выиграшных комбинаций не может быть
+        if self.desk.step > 3:
+            state = self.desk.check_desk()
+            if state[0]:
+                self.show_results(state)
+
+    def show_results(self, state):
         """ Уведомляет о резульатах игры """
-        self.view.results(winner)
+        self.view.results(state)
 
 
 class View:
@@ -182,7 +226,7 @@ class View:
 
     def results(self, state):
         if state[1]:
-            self.notify(f"Game over: {state[2].name} won this game!")
+            self.notify(f"Game over: {state[1].name} won this game!")
         else:
             self.notify(f"Game over: Draw!")
 
@@ -197,7 +241,8 @@ class Game:
         self.run = True
         self.controller.init_players()
         while self.run:
-            self.controller.read()
+            self.run = self.controller.read()
+        input()     # Чтобы окно не закрывалось моментально после итогов игры
 
 
 if __name__ == "__main__":
